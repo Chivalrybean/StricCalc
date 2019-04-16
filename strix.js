b77306c19f411c475126fce6a92f7dea2c70131e
Chivalrybean <44534661+Chivalrybean@users.noreply.github.com>
2019-04-15 22:15:02 -0700

    Initial upload

---
+++ strix.js
@@ -0,0 +1,12 @@
+function calc(damage, cool) {
+    var x = damage * 600 / (cool - 1);
+    var y = damage * 1.2 * 600 / (cool + 1);
+    //return "Panic will output " + (damage * 600 / (cool - 1)) + "damage every 10 minutes where Hide will output" + (damage * 600 / (cool + 1)) + "every 10 minutes.";
+    return "Panic will output " + x + " damage every 10 minutes where Hide will output " + y + " every 10 minutes.";
+}
+function press() {
+    //let x = +document.getElementById("damageInput").innerText
+    //let y = +document.getElementById("cooldownInput").innerText
+    var resultString = calc(+document.getElementById("damageInput").value, +document.getElementById("cooldownInput").value);
+    document.getElementById("result").innerText = resultString;
+}
---
+++ strix.ts
@@ -0,0 +1,22 @@
+function calc(damage: number, cool: number) {
+
+  let x = damage * 600 / (cool - 1)
+  let y = damage * 1.2 * 600 / (cool + 1)
+
+  
+  //return "Panic will output " + (damage * 600 / (cool - 1)) + "damage every 10 minutes where Hide will output" + (damage * 600 / (cool + 1)) + "every 10 minutes.";
+  return "Panic will output " + x + " damage every 10 minutes where Hide will output " + y + " every 10 minutes.";
+}
+
+function press() {
+  //let x = +document.getElementById("damageInput").innerText
+  //let y = +document.getElementById("cooldownInput").innerText
+
+
+  let resultString = calc(+(<HTMLInputElement>document.getElementById("damageInput")).value, +(<HTMLInputElement>document.getElementById("cooldownInput")).value);
+
+  document.getElementById("result").innerText = resultString;
+}
+ 
+
+
