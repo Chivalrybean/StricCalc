e836b1b6fbc731577934186285bca31072f77729
Chivalrybean <jlowmaster@gmail.com>
2019-04-15 22:47:57 -0700

    Text updates lost in a previous disaster (I'll get up version control sooner now ...)

--- strix.js
+++ strix.js
@@ -2,7 +2,7 @@ function calc(damage, cool) {
     var x = damage * 600 / (cool - 1);
     var y = damage * 1.2 * 600 / (cool + 1);
     //return "Panic will output " + (damage * 600 / (cool - 1)) + "damage every 10 minutes where Hide will output" + (damage * 600 / (cool + 1)) + "every 10 minutes.";
-    return "Panic will output " + x + " damage every 10 minutes where Hide will output " + y + " every 10 minutes.";
+    return "Panic will output " + x + " damage every 10 minutes (while allies under attack) where Hide will output " + y + " every 10 minutes.";
 }
 function press() {
     //let x = +document.getElementById("damageInput").innerText
--- strix.ts
+++ strix.ts
@@ -1,11 +1,11 @@
 function calc(damage: number, cool: number) {
 
-  let x = damage * 600 / (cool - 1)
-  let y = damage * 1.2 * 600 / (cool + 1)
+  let x = damage * 600 / (cool - 1);
+  let y = damage * 1.2 * 600 / (cool + 1);
 
   
   //return "Panic will output " + (damage * 600 / (cool - 1)) + "damage every 10 minutes where Hide will output" + (damage * 600 / (cool + 1)) + "every 10 minutes.";
-  return "Panic will output " + x + " damage every 10 minutes where Hide will output " + y + " every 10 minutes.";
+  return "Panic will output " + x + " damage every 10 minutes (while allies under attack) where Hide will output " + y + " every 10 minutes.";
 }
 
 function press() {
